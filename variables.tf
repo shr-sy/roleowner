7ce7fec24025831d8d3fa444f68ea2c3e7301d42
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-07-24 11:24:57 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "snowflake_account" {
+  description = "Snowflake account identifier"
+  type        = string
+}
+
+variable "snowflake_username" {
+  description = "Snowflake username"
+  type        = string
+}
+
+variable "snowflake_password" {
+  description = "Snowflake password"
+  type        = string
+  sensitive   = true
+}
+
+variable "target_role" {
+  description = "Role whose ownership needs to be changed"
+  type        = string
+  default     = "HCPUSER"
+}
+
+variable "new_owner" {
+  description = "New owner of the target role"
+  type        = string
+  default     = "SYSADMIN"
+}
