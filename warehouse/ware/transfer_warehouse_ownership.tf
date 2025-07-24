6c047ea5ff292b2267e4c38802c2343a839a54cc
Shruti Gupta <82036296+shr-sy@users.noreply.github.com>
2025-07-24 15:12:53 +0530

    Update transfer_warehouse_ownership.tf

--- warehouse/ware/transfer_warehouse_ownership.tf
+++ warehouse/ware/transfer_warehouse_ownership.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "Snowflake-Labs/snowflake"
+      version = "~> 0.73.0"
+    }
+  }
+}
+
 resource "snowflake_warehouse_ownership_grant" "warehouse_ownership_transfer" {
   on_warehouse_name = var.target_warehouse
   to_role_name      = var.new_owner
